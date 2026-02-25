c3612e015f543b89ccb907ed22114337f23b0828
wildvchen <wildfpsval@gmail.com>
2026-02-25 08:44:54 -0500

    create a base for intake, transfer, and outtake using buttons such as a for intake b for outtake x transfer Plans: change out take power depending on distance from goal using limelight

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/PrometheusDriver.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/PrometheusDriver.java
@@ -9,9 +9,16 @@ public class PrometheusDriver extends LinearOpMode {
 
     private DcMotor leftFrontDrive, leftBackDrive, rightFrontDrive, rightBackDrive;
     private DcMotor intakeMotor1, intakeMotor2;
+    private DcMotor outtakeMotor1, outtakeMotor2;
+    private DcMotor transferMotor;
 
     private boolean intakeOn = false;
+    private boolean outtakeOn = false;
+    private boolean transferOn = false;
+
     private boolean lastAState = false;
+    private boolean lastBState = false;
+    private boolean lastXState = false;
 
     @Override
     public void runOpMode() {
@@ -23,6 +30,9 @@ public class PrometheusDriver extends LinearOpMode {
 
         intakeMotor1 = hardwareMap.get(DcMotor.class, "intake_motor1");
         intakeMotor2 = hardwareMap.get(DcMotor.class, "intake_motor2");
+        outtakeMotor1 = hardwareMap.get(DcMotor.class, "outtake_motor1");
+        outtakeMotor2 = hardwareMap.get(DcMotor.class, "outtake_motor2");
+        transferMotor = hardwareMap.get(DcMotor.class, "transfer_motor");
 
         // Set directions
         leftFrontDrive.setDirection(DcMotor.Direction.FORWARD);
@@ -32,6 +42,11 @@ public class PrometheusDriver extends LinearOpMode {
         
         intakeMotor1.setDirection(DcMotor.Direction.FORWARD);
         intakeMotor2.setDirection(DcMotor.Direction.REVERSE);
+
+        outtakeMotor1.setDirection(DcMotor.Direction.FORWARD);
+        outtakeMotor2.setDirection(DcMotor.Direction.FORWARD);
+        
+        transferMotor.setDirection(DcMotor.Direction.FORWARD);
 
         telemetry.addData("Status", "Initialized");
         telemetry.update();
@@ -55,22 +70,39 @@ public class PrometheusDriver extends LinearOpMode {
             rightFrontDrive.setPower(frontRightPower);
             rightBackDrive.setPower(backRightPower);
 
-            // --- Intake Control (Toggle) ---
+            // --- Intake Control (Toggle - Button A) ---
             if (gamepad1.a && !lastAState) {
                 intakeOn = !intakeOn;
             }
             lastAState = gamepad1.a;
 
-            if (intakeOn) {
-                intakeMotor1.setPower(1.0);
-                intakeMotor2.setPower(1.0);
-            } else {
-                intakeMotor1.setPower(0.0);
-                intakeMotor2.setPower(0.0);
+            // --- Outtake/Flywheel Control (Toggle - Button B) ---
+            if (gamepad1.b && !lastBState) {
+                outtakeOn = !outtakeOn;
             }
+            lastBState = gamepad1.b;
+
+            // --- Transfer Wheels Control (Toggle - Button X) ---
+            if (gamepad1.x && !lastXState) {
+                transferOn = !transferOn;
+            }
+            lastXState = gamepad1.x;
+
+            // Apply power to Intake Motors
+            intakeMotor1.setPower(intakeOn ? 1.0 : 0.0);
+            intakeMotor2.setPower(intakeOn ? 1.0 : 0.0);
+
+            // Apply power to Outtake (Flywheel) Motors
+            outtakeMotor1.setPower(outtakeOn ? 1.0 : 0.0);
+            outtakeMotor2.setPower(outtakeOn ? 1.0 : 0.0);
+            
+            // Apply power to Transfer Motor
+            transferMotor.setPower(transferOn ? 1.0 : 0.0);
 
             telemetry.addData("Status", "Running");
-            telemetry.addData("Intake", intakeOn ? "ON (Toggle)" : "OFF (Toggle)");
+            telemetry.addData("Intake", intakeOn ? "ON" : "OFF");
+            telemetry.addData("Transfer", transferOn ? "ON" : "OFF");
+            telemetry.addData("Outtake (Flywheel)", outtakeOn ? "ON" : "OFF");
             telemetry.update();
         }
     }
